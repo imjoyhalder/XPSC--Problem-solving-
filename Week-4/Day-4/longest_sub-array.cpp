af84cc081aed1f10bbe3506fecf4c607b5c8a2b9
JOY <117473600+imjoyhalder@users.noreply.github.com>
2024-11-20 23:10:04 +0600

    Add files via upload
    week-4 day-4 codes added

---
+++ Week-4/Day-4/longest_sub-array.cpp
@@ -0,0 +1,39 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution{
+  public:
+    int longestKSubstr(string s, int k) {
+        
+        int n=s.size(),l=0,r=0,ans=-1;
+        map<char,int>mp;
+        while(r<n)
+        {
+            mp[s[r]]++;
+            if(mp.size()==k)
+            {
+                ans=max(ans,r-l+1);
+            }
+            else 
+            {
+                while(mp.size()>k && l<=r)
+                {
+                    mp[s[l]]--;
+                    if(mp[s[l]]==0)
+                    {
+                        mp.erase(s[l]);
+                    }
+                    l++;
+                }
+            }
+            r++;
+        }
+        return ans;
+    }
+};
+
+int main(){
+
+}
