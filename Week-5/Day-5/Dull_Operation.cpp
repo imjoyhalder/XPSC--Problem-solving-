937498df44aa98d4ab1249c498c81175676c7323
JOY <117473600+imjoyhalder@users.noreply.github.com>
2024-11-28 23:27:59 +0600

    Add files via upload

---
+++ Week-5/Day-5/Dull_Operation.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define nl '\n'
+#define all(v) v.begin(),v.end()
+#define print(v) for(auto data: v) cout<<data<<' '
+#define yes cout<<"YES"<<nl; 
+#define no cout<<"NO"<<nl; 
+using namespace std;
+
+void solve(){
+    
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); 
+       
+    //cout<<(3|4)*(3^4)<<nl; 
+    int t; cin>>t; 
+    while(t--){
+        int n; cin>>n; 
+        
+        cout<<n<<" "<<n-1<<nl; 
+        
+    }
+   
+    return 0;
+} 
